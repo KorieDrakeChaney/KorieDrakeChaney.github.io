86cced50311396366554570ee244e32d5cd22408
KorieDrakeChaney <KorieDrakeChaney@gmail.com>
2021-12-20 19:53:05 -0600

    init

---
+++ src/App.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { Styles } from 'sveltestrap';
+	import Nav from "./Nav/Nav.svelte";
+</script>
+
+<Nav />
+
+
+
+<main>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<Styles />
---
+++ src/Nav/Nav.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+
+  import { 
+        Nav,
+        Navbar, 
+        NavItem, 
+        NavLink, 
+        NavbarBrand
+  } from 'sveltestrap';
+
+</script>
+  <Navbar color="light" light expand="md">
+    <NavbarBrand href="https://github.com/KorieDrakeChaney">KorieDrakeChaney</NavbarBrand>
+    <Nav tabs class="ms-auto" navbar>
+        <NavItem>
+            <NavLink href="/Blogs">Blogs</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/About">About</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/Portfolio">Portfolio</NavLink>
+          </NavItem>
+    </Nav>
+  </Navbar>
